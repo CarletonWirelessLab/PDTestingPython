f1024caaef7ac4f0ca365090949369ac3a643d3a
JeffreyAJBailey <68293235+JeffreyAJBailey@users.noreply.github.com>
2020-08-26 15:18:35 -0400

    Add files via upload

--- decode_type.py
+++ decode_type.py
@@ -7,7 +7,8 @@ def decode_type(sub_type_field):
     # 00001000 is the first octet of the frame control field for beacon subtype packets (first two bits are clipped off)
     if np.array_equal(sub_type_field, [0,0,1,0,0,0]):
         packet_type = 'Beacon'
+    elif np.array_equal(sub_type_field, [1,0,0,0,0,0]):
+        packet_type = 'Data'
     else:
-        packet_type = "N/A"
-
+        packet_type = 'N/A'
     return packet_type
